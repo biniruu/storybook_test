93847d2c7bd516f6453a0da5862097cec5ba1048
biniruu <biniruu@gmail.com>
2022-06-30 19:46:00 +0900

    기능은 대충 구현

--- src/stories/Chartjs.jsx
+++ src/stories/Chartjs.jsx
@@ -5,57 +5,56 @@ import { Chart, getElementAtEvent } from 'react-chartjs-2'
 import { faker } from '@faker-js/faker'
 ChartJS.register(...registerables)
 
-import { Slider } from 'antd'
+import { Slider, Button } from 'antd'
 
 export const Chartjs = () => {
-  const onChangeSlider = position => {
-    console.log('position:', position)
+  const [position, setPosition] = useState(0)
+  const updatePosition = value => {
+    if (value > 792 || value < 0) {
+      return false
+    }
+    setPosition(value)
+    // ref.current.update()
   }
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    // 'November',
-    // 'December',
-  ]
+  const onChangeSlider = val => {
+    console.log('position:', val)
+    updatePosition(val)
+  }
 
-  const arr = Array(300)
+  const labels = Array(9000)
     .fill(0)
-    .reduce(acc => {
-      const arrData = [
-        {
-          type: 'line',
-          label: 'Dataset 1',
-          borderColor: 'rgb(255, 99, 132)',
-          borderWidth: 2,
-          fill: false,
-          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        },
-        {
-          type: 'bar',
-          label: 'Dataset 2',
-          backgroundColor: 'rgb(75, 192, 192)',
-          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-          borderColor: 'white',
-          borderWidth: 2,
-        },
-        {
-          type: 'bar',
-          label: 'Dataset 3',
-          backgroundColor: 'rgb(53, 162, 235)',
-          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        },
-      ]
-      return [...acc, ...arrData]
-    }, [])
-  console.log(arr.length)
+    .map(() => faker.datatype.number({ min: 100, max: 10000 }))
+
+  // const arr = Array(1)
+  //   .fill(0)
+  //   .reduce(acc => {
+  //     const arrData = [
+  //       {
+  //         type: 'line',
+  //         label: 'Dataset 1',
+  //         borderColor: 'rgb(255, 99, 132)',
+  //         borderWidth: 2,
+  //         fill: false,
+  //         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       },
+  //       {
+  //         type: 'bar',
+  //         label: 'Dataset 2',
+  //         backgroundColor: 'rgb(75, 192, 192)',
+  //         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //         borderColor: 'white',
+  //         borderWidth: 2,
+  //       },
+  //       {
+  //         type: 'bar',
+  //         label: 'Dataset 3',
+  //         backgroundColor: 'rgb(53, 162, 235)',
+  //         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       },
+  //     ]
+  //     return [...acc, ...arrData]
+  //   }, [])
+
   const data = {
     labels,
     // datasets: [
@@ -82,63 +81,102 @@ export const Chartjs = () => {
     //     data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
     //   },
     // ],
-    datasets: arr,
+    datasets: [
+      {
+        type: 'line',
+        label: 'Dataset 1',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 2,
+        fill: false,
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      },
+      {
+        type: 'bar',
+        label: 'Dataset 2',
+        backgroundColor: 'rgb(75, 192, 192)',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: 'white',
+        borderWidth: 2,
+      },
+      {
+        type: 'bar',
+        label: 'Dataset 3',
+        backgroundColor: 'rgb(53, 162, 235)',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      },
+    ],
   }
 
-  const moveChart = {
-    id: 'moveChart',
-    afterDraw(chart, args, pluginOptions) {
-      const {
-        ctx,
-        chartArea: { left, right, top, bottom, width, height },
-      } = chart
-      // console.log(chart)
+  // const moveChart = {
+  //   id: 'moveChart',
+  //   afterDraw(chart, args, pluginOptions) {
+  //     const {
+  //       ctx,
+  //       chartArea: { left, right, top, bottom, width, height },
+  //     } = chart
+  //     // console.log(chart)
+  //   },
+  // }
+
+  const options = {
+    scales: {
+      x: {
+        min: 0,
+        max: 365,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+    onClick(evt) {
+      const { ctx, canvas } = ref.current
+      const x = evt.x
+      const y = evt.y
+      console.log('x:', x)
+      console.log('y:', y)
     },
   }
 
-  // const options = {
-  //   scales: {
-  //     x: {
-  //       min: 0,
-  //       max: 10,
-  //     },
-  //     y: {
-  //       beginAtZero: true,
-  //     },
-  //   },
-  //   // onClick() {
-  //   //   console.log('click')
-  //   // },
-  // }
-
   const ref = useRef()
 
-  const onClick = evt => {
-    // console.log('ref fired')
-    console.log(getElementAtEvent(ref.current, evt))
+  const btnClick = () => {
+    updatePosition(position + 1)
+    console.log(options.scales.x)
   }
 
   useEffect(() => {
     ref.current.update()
-    const moveScroll = () => {
-      const { ctx, canvas } = ref.current
-      canvas.addEventListener('click', evt => {
-        const rect = canvas.getBoundingClientRect()
-        const x = (e.clientX = rect.left)
-        const y = (e.clientY = rect.top)
-        console.log('x:', x)
-        console.log('y:', y)
-      })
-    }
-    ref.current.ctx.onclick = moveScroll()
   }, [])
+
+  useEffect(() => {
+    if (options.scales.x.min < position) {
+      options.scales.x.max = options.scales.x.max + position
+    } else {
+      options.scales.x.max = options.scales.x.max - position
+    }
+    options.scales.x.min = position
+    ref.current.update()
+  }, [position])
 
   return (
     <div className={'container'}>
       <div className="chart-container">
-        <Chart id="chart" className={'chart'} type="bar" ref={ref} data={data} plugins={[moveChart]}></Chart>
+        <Chart
+          id="chart"
+          className={'chart'}
+          type="bar"
+          ref={ref}
+          data={data}
+          // plugins={[moveChart]}
+          options={options}
+        ></Chart>
         <div className="chart-container">
-          <Slider defaultValue={5} onChange={onChangeSlider} />
+          <Slider defaultValue={position} value={position} min={0} max={792} onChange={onChangeSlider} />
+        </div>
+        <div className="button-container">
+          <Button type="primary" onClick={btnClick}>
+            next
+          </Button>
         </div>
       </div>
     </div>
