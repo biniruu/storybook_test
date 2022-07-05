99fe3af7ad5d2955ddb89a91b27a9f08006b5014
biniruu <biniruu@gmail.com>
2022-07-05 11:16:09 +0900

    클릭 데이터

--- src/stories/Chartjs.jsx
+++ src/stories/Chartjs.jsx
@@ -1,23 +1,32 @@
-import React, { useRef, useState, useEffect } from 'react'
 import './chartjs.css'
+
+import { Button, Slider } from 'antd'
 import { Chart as ChartJS, registerables } from 'chart.js'
+import React, { useEffect, useRef, useState } from 'react'
+
 import { Chart } from 'react-chartjs-2'
 import { faker } from '@faker-js/faker'
-ChartJS.register(...registerables)
 
-import { Slider, Button } from 'antd'
+ChartJS.register(...registerables)
 
 export const Chartjs = () => {
   const [position, setPosition] = useState(0)
+
   const updatePosition = value => {
     if (value <= 8977 && value >= 0) {
       setPosition(value)
     }
   }
+
   // const onChangeSlider = val => {
   //   console.log('position:', val)
   //   updatePosition(val)
   // }
+
+  const handleEvent = val => {
+    console.log('handleEvent:', val)
+    updatePosition(val)
+  }
 
   const labels = Array(9000)
     .fill(0)
@@ -27,17 +36,14 @@ export const Chartjs = () => {
       return acc
     }, [])
 
-  const handleEvent = val => {
-    console.log('handleEvent:', val)
-    updatePosition(val)
-  }
+  const scatter = ['undefined', 'undefined', 'undefined', 30, 50]
 
   const data = {
     labels,
     datasets: [
       {
         type: 'line',
-        label: 'Dataset 1',
+        label: '회전익동',
         borderColor: 'rgb(255, 99, 132)',
         borderWidth: 2,
         fill: false,
@@ -45,7 +51,7 @@ export const Chartjs = () => {
       },
       {
         type: 'bar',
-        label: 'Dataset 2',
+        label: '설비동',
         backgroundColor: 'rgb(75, 192, 192)',
         data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
         borderColor: 'white',
@@ -53,15 +59,15 @@ export const Chartjs = () => {
       },
       {
         type: 'bar',
-        label: 'Dataset 3',
+        label: '봉천동',
         backgroundColor: 'rgb(53, 162, 235)',
         data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
       },
       {
         type: 'scatter',
-        label: 'Dataset 4',
+        label: '규동',
         backgroundColor: 'rgb(250, 173, 20)',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        data: scatter,
       },
     ],
   }
@@ -77,40 +83,6 @@ export const Chartjs = () => {
   //   },
   // }
 
-  const maxScale = 23
-
-  const options = {
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        min: 0,
-        max: maxScale,
-        // ticks: {
-        //   // maxTicksLimit: maxScale,
-        //   callback(val, idx) {
-        //     // console.log('ticks', val, idx)
-        //     return this.getLabelForValue(val)
-        //     // return idx % 2 === 0 ? this.getLabelForValue(val) : ''
-        //   },
-        // },
-      },
-      y: {
-        beginAtZero: true,
-        ticks: {
-          // forces step size to be 50 units
-          stepSize: 25,
-        },
-      },
-    },
-    onClick(evt) {
-      const { ctx, canvas } = ref.current
-      const x = evt.x
-      const y = evt.y
-    },
-  }
-
-  const ref = useRef()
-
   const btnClick = () => {
     console.log('btnClick', ref.current)
     updatePosition(position + 1)
@@ -125,6 +97,43 @@ export const Chartjs = () => {
     ref.current.config.options.scales.x.max = position + maxScale
     ref.current.update()
   }, [position])
+
+  const maxScale = 23
+
+  const options = {
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        min: 0,
+        max: maxScale,
+        ticks: {
+          // maxTicksLimit: maxScale,
+          callback(val, idx) {
+            // console.log('ticks', val, idx)
+            return this.getLabelForValue(val)
+          },
+          z: 9,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          // forces step size to be 50 units
+          stepSize: 25,
+        },
+      },
+    },
+    onClick(evt) {
+      const { ctx, canvas } = ref.current
+      const x = evt.x
+      const y = evt.y
+      const dataset = ref.current.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, false)
+      const label = data.datasets[dataset[0].datasetIndex].label
+      console.log(label)
+    },
+  }
+
+  const ref = useRef()
 
   return (
     <div className={'container'}>
