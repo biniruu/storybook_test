4880b3bd8d0066f147e0f01c8909e6b01afdcb6a
biniruu <biniruu@gmail.com>
2022-07-11 12:05:35 +0900

    불필요한 주석 제거

--- src/stories/Recharts.jsx
+++ src/stories/Recharts.jsx
@@ -13,12 +13,10 @@ import {
   YAxis,
 } from 'recharts'
 import { Button, Slider } from 'antd'
-import React, { memo, useMemo, useRef, useState } from 'react'
+import React, { memo, useState } from 'react'
 
 import Factory from './assets/factory.png'
 import { faker } from '@faker-js/faker'
-
-console.log('labels fired')
 
 const _lables = Array(9000)
   .fill(0)
@@ -31,14 +29,10 @@ const _lables = Array(9000)
       warning: false,
     }
     acc.push(obj)
-    // acc.push(index + 1)
     return acc
   }, [])
-// return _lables
 
 const division = (배열, n개씩) => {
-  console.log('labels fired')
-
   const length = 배열.length
   const divide = Math.floor(length / n개씩) + (Math.floor(length % n개씩) > 0 ? 1 : 0)
   const newArray = []
@@ -54,7 +48,6 @@ const labels = division(_lables, 24)
 
 const marks = {}
 
-console.log('labels:', labels)
 labels[130][0].warning = true
 labels[180][0].warning = true
 labels[230][0].warning = true
@@ -69,58 +62,7 @@ labels.forEach((arr, idx) => {
 })
 
 const RenderLineChart = memo(({ current }) => {
-  console.log('current', current)
-  // const labels = useMemo(() => {
-  //   console.log('labels fired')
-  //   const _lables = Array(90)
-  //     .fill(0)
-  //     .reduce((acc, curr, idx) => {
-  //       const obj = {
-  //         name: `D+${idx + 1}`,
-  //         uv: faker.datatype.number({ min: 0, max: 1800 }),
-  //         pv: faker.datatype.number({ min: 0, max: 1800 }),
-  //         amt: faker.datatype.number({ min: 0, max: 1800 }),
-  //       }
-  //       acc.push(obj)
-  //       // acc.push(index + 1)
-  //       return acc
-  //     }, [])
-  //   // return _lables
-
-  //   const division = (배열, n개씩) => {
-  //     const length = 배열.length
-  //     const divide = Math.floor(length / n개씩) + (Math.floor(length % n개씩) > 0 ? 1 : 0)
-  //     const newArray = []
-
-  //     for (let i = 0; i <= divide; i++) {
-  //       // 배열 0부터 n개씩 잘라 새 배열에 넣기
-  //       newArray.push(배열.splice(0, n개씩))
-  //     }
-
-  //     return newArray
-  //   }
-
-  //   const result = division(_lables, 24)
-
-  //   return result
-  // }, [])
-
-  console.log('labels:', labels)
-
-  // const fakeData = useMemo(() => {
-  //   const numbers = labels.map(() => faker.datatype.number({ min: 0, max: 1800 }))
-  //   return numbers
-  // }, [])
-
-  // const data = labels.map((item, i) => ({
-  //   name: labels[i],
-  //   uv: faker.datatype.number({ min: 0, max: 1800 }),
-  //   pv: faker.datatype.number({ min: 0, max: 1800 }),
-  //   amt: faker.datatype.number({ min: 0, max: 1800 }),
-  // }))
   const data = labels[current]
-
-  console.log('data:', data)
 
   return (
     <ResponsiveContainer width="100%" height="100%" debounce={300}>
@@ -163,31 +105,25 @@ export const Recharts = memo(() => {
   }
 
   const handleEvent = val => {
-    console.log('handleEvent:', val)
     updateSliderValue(val)
     setWarnChart1('')
     setWarnChart2('')
     setWarnChart3('')
     setWarnChart4('')
-    console.log('handleEvent:', val)
     if (val > 100 && val <= 150) {
       setWarnChart1('active')
-      console.log('setWarnChart1 fired')
       return false
     }
     if (val > 150 && val <= 200) {
       setWarnChart2('active')
-      console.log('setWarnChart2 fired')
       return false
     }
     if (val > 200 && val <= 250) {
       setWarnChart3('active')
-      console.log('setWarnChart3 fired')
       return false
     }
     if (val > 250 && val <= 300) {
       setWarnChart4('active')
-      console.log('setWarnChart4 fired')
       return false
     }
     console.log('switch fired')
@@ -200,17 +136,6 @@ export const Recharts = memo(() => {
     handleEvent(value - 1)
   }
 
-  // const marks = {
-  //   300: 'warning1',
-  //   1800: {
-  //     style: {
-  //       top: '0px',
-  //     },
-  //     label: 'warning2',
-  //   },
-  //   2200: 'warning3',
-  //   3400: 'warning4',
-  // }
   const tooltip = value => `position ${value + 1}`
 
   return (
